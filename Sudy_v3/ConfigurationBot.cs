5d04e8c47c8294370d94e5a09d3218b63d43438c
Максим Лебединцев <dypakin@gmail.com>
2024-01-14 23:00:30 +0400

    fix config

--- Sudy_v3/ConfigurationBot.cs
+++ Sudy_v3/ConfigurationBot.cs
@@ -1,7 +1,6 @@
 ﻿public class ConfigurationBot
 {
     public string? Token { get; set; }
-    public string? Prefix { get; set; }
     public string? Currently { get; set; }
 
     public string? StatusText { get; set; }
