8a7f0c67715741cc7823eb41f963d2e46be6e1dd
Максим Лебединцев <dypakin@gmail.com>
2024-03-18 23:40:48 +0400

    update ffmpeg

---
+++ Sudy_v3/FFmpegDyHelper/FFmpegDy.cs
@@ -0,0 +1,268 @@
+﻿using FFmpeg.AutoGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudy_v3.FFmpegDyHelper
+{
+    public static unsafe class FFmpegDy
+    {
+        [DllImport("Kernel32.dll", EntryPoint = "RtlMoveMemory", SetLastError = false)]
+        static extern void MoveMemory(IntPtr dest, IntPtr src, int size);
+
+        const int AUDIO_INBUF_SIZE = 20480;
+        const int AUDIO_REFILL_THRESH = 4096;
+
+        static unsafe public void decode_audio(string inputFileName, string outputFileName)
+        {
+            AVCodec* codec = null;
+            AVPacket* packet = null;
+            AVCodecParserContext* parser = null;
+            AVCodecContext* c = null;
+            AVFrame* decoded_frame = null;
+
+            int ret;
+
+            do
+            {
+                packet = ffmpeg.av_packet_alloc();
+
+                codec = ffmpeg.avcodec_find_decoder(AVCodecID.AV_CODEC_ID_MP3);
+                if (codec == null)
+                {
+                    Console.WriteLine("Codec not found");
+                    break;
+                }
+
+                parser = ffmpeg.av_parser_init((int)codec->id);
+                if (parser == null)
+                {
+                    Console.WriteLine("Parser not found");
+                    break;
+                }
+
+                c = ffmpeg.avcodec_alloc_context3(codec);
+                if (c == null)
+                {
+                    Console.WriteLine("Could not allocate audio codec context");
+                    break;
+                }
+
+                c->sample_rate = 48000;
+
+
+                if (ffmpeg.avcodec_open2(c, codec, null) < 0)
+                {
+                    Console.WriteLine("Could not open codec");
+                    break;
+                }
+
+                using FileStream f = File.OpenRead(inputFileName);
+                using FileStream outfile = File.OpenWrite(outputFileName);
+
+                byte[] inbuf = new byte[AUDIO_INBUF_SIZE + ffmpeg.AV_INPUT_BUFFER_PADDING_SIZE];
+                fixed (byte* ptr = &inbuf[0])
+                {
+                    byte* data = ptr;
+                    int data_size = f.Read(inbuf, 0, AUDIO_INBUF_SIZE);
+
+                    while (data_size > 0)
+                    {
+                        if (decoded_frame == null)
+                        {
+                            decoded_frame = ffmpeg.av_frame_alloc();
+                            if (decoded_frame == null)
+                            {
+                                Console.WriteLine("Could not allocate audio frame");
+                                break;
+                            }
+                        }
+
+                        ret = ffmpeg.av_parser_parse2(parser, c, &packet->data, &packet->size,
+                            data, data_size, ffmpeg.AV_NOPTS_VALUE, ffmpeg.AV_NOPTS_VALUE, 0);
+
+                        if (ret < 0)
+                        {
+                            Console.WriteLine("Error while parsing");
+                            break;
+                        }
+
+                        data += ret;
+                        data_size -= ret;
+
+                        if (packet->size != 0)
+                        {
+                            if (decode(c, packet, decoded_frame, outfile) == false)
+                            {
+                                break;
+                            }
+                        }
+
+                        if (data_size < AUDIO_REFILL_THRESH)
+                        {
+                            fixed (byte* temp_ptr = &inbuf[0])
+                            {
+                                MoveMemory(new IntPtr(temp_ptr), new IntPtr(data), data_size);
+                                data = temp_ptr;
+
+                                int len = f.Read(inbuf, data_size, AUDIO_INBUF_SIZE - data_size);
+                                if (len > 0)
+                                {
+                                    data_size += len;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                packet->data = null;
+                packet->size = 0;
+
+                if (decode(c, packet, decoded_frame, outfile) == false)
+                {
+                    break;
+                }
+
+                AVSampleFormat sfmt = AVSampleFormat.AV_SAMPLE_FMT_S16; // c->sample_fmt;
+
+                if (ffmpeg.av_sample_fmt_is_planar(sfmt) != 0)
+                {
+                    string packed = ffmpeg.av_get_sample_fmt_name(sfmt);
+                    Console.WriteLine($"Warning: the sample format the decoder produced is planar {0}. This example will output the first channel only.",
+                        packed == null ? "?" : packed);
+                    sfmt = ffmpeg.av_get_packed_sample_fmt(sfmt);
+                }
+
+                int n_channels = c->channels;
+                string fmt = get_format_from_sample_fmt(sfmt);
+                if (fmt == null)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Play the output audio file with the command:\n" +
+                    $"ffplay -f {fmt} -ac {n_channels} -ar {c->sample_rate} {outputFileName}\n");
+
+            } while (false);
+
+            if (c != null)
+            {
+                ffmpeg.avcodec_free_context(&c);
+            }
+
+            if (parser != null)
+            {
+                ffmpeg.av_parser_close(parser);
+            }
+
+            if (decoded_frame != null)
+            {
+                ffmpeg.av_frame_free(&decoded_frame);
+            }
+
+            if (packet != null)
+            {
+                ffmpeg.av_packet_free(&packet);
+            }
+        }
+
+        private unsafe static bool decode(AVCodecContext* dec_ctx, AVPacket* packet, AVFrame* frame, FileStream outfile)
+        {
+            int i;
+            int ch;
+            int ret;
+            int data_size;
+
+            ret = ffmpeg.avcodec_send_packet(dec_ctx, packet);
+            if (ret < 0)
+            {
+                Console.WriteLine($"Error submitting the packet to the decoder: {ret}");
+                return true;
+            }
+
+            while (ret >= 0)
+            {
+                ret = ffmpeg.avcodec_receive_frame(dec_ctx, frame);
+                if (ret == ffmpeg.AVERROR(ffmpeg.EAGAIN) || ret == ffmpeg.AVERROR_EOF)
+                {
+                    return true;
+                }
+                else if (ret < 0)
+                {
+                    Console.WriteLine("Error during decoding");
+                    return false;
+                }
+
+                data_size = ffmpeg.av_get_bytes_per_sample(AVSampleFormat.AV_SAMPLE_FMT_S16);//dec_ctx->sample_fmt);
+                if (data_size < 0)
+                {
+                    Console.WriteLine("Failed to calculate data size");
+                    return false;
+                }
+
+                for (i = 0; i < frame->nb_samples; i++)
+                {
+                    for (ch = 0; ch < dec_ctx->channels; ch++)
+                    {
+                        byte* ptr = frame->data[0];
+                        ptr += (data_size * i);
+
+                        ReadOnlySpan<byte> data = new ReadOnlySpan<byte>(ptr, data_size);
+                        outfile.Write(data);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        static unsafe string get_format_from_sample_fmt(AVSampleFormat sample_fmt)
+        {
+            sample_fmt_entry[] sample_fmt_entries = {
+                new sample_fmt_entry(AVSampleFormat.AV_SAMPLE_FMT_U8, "u8", "u8"),
+                new sample_fmt_entry(AVSampleFormat.AV_SAMPLE_FMT_S16, "s16be", "s16le"),
+                new sample_fmt_entry(AVSampleFormat.AV_SAMPLE_FMT_S32, "s32be", "s32le"),
+                new sample_fmt_entry(AVSampleFormat.AV_SAMPLE_FMT_FLT, "f32be", "f32le"),
+                new sample_fmt_entry(AVSampleFormat.AV_SAMPLE_FMT_DBL, "f64be", "f64le"),
+            };
+
+            foreach (var entry in sample_fmt_entries)
+            {
+                if (entry.SampleFormat == sample_fmt)
+                {
+                    if (BitConverter.IsLittleEndian == true)
+                    {
+                        return entry.FormatLE;
+                    }
+
+                    return entry.FoamtBE;
+                }
+            }
+
+            Console.WriteLine($"sample format {ffmpeg.av_get_sample_fmt_name(sample_fmt)} is not supported as output format");
+            return null;
+        }
+    }
+
+    struct sample_fmt_entry
+    {
+        AVSampleFormat sample_fmt;
+        public AVSampleFormat SampleFormat => sample_fmt;
+
+        string fmt_be;
+        public string FoamtBE => fmt_be;
+
+        string fmt_le;
+        public string FormatLE => fmt_le;
+
+        public sample_fmt_entry(AVSampleFormat fmt, string be, string le)
+        {
+            sample_fmt = fmt;
+            fmt_be = be;
+            fmt_le = le;
+        }
+    }
+}
--- Sudy_v3/Modules/AudioCommands.cs
+++ Sudy_v3/Modules/AudioCommands.cs
@@ -5,7 +5,10 @@ using Discord.Commands;
 using Discord.Interactions;
 using FFmpeg.AutoGen;
 using Microsoft.Extensions.DependencyInjection;
+using Sudy_v3.FFmpegDyHelper;
 using System.Diagnostics;
+using System.Net.Sockets;
+using System.Runtime.Intrinsics.X86;
 using VideoLibrary;
 
 namespace Sudy_v3.Modules
@@ -28,75 +31,45 @@ namespace Sudy_v3.Modules
 
             IAudioClient? audio = await channel.ConnectAsync();
 
-            await SendAsync(audio, $@"{AppDomain.CurrentDomain.BaseDirectory}{_config.LocalStorage.Music}uwu-very.mp3");
+            await SendAsync(audio, $@"{AppDomain.CurrentDomain.BaseDirectory}{_config.LocalStorage.Music}test_norm_samle_48000_s16le.dat");//uwu-very.mp3");
 
         }
-
-
-        unsafe static AVFormatContext* _pAvFormatInputFileContext = null;
-        unsafe static AVFormatContext* _pAvFormatOutputFileContext = null;
 
         [SlashCommand("play", "Проиграть музыку.")]
         public async Task PlayMusic(string url)
         {
+
             var channel = (Context.User as IGuildUser)?.VoiceChannel ?? null;
 
             if (channel == null) { await RespondAsync($"А, {Context.User.Mention}, выйдет?"); return; }
 
             IAudioClient? audio = await channel.ConnectAsync();
 
-            YouTube youtube = YouTube.Default;
+            //YouTube youtube = YouTube.Default;
 
-            var v = youtube.GetVideo(@"https://www.youtube.com/watch?v=__-vp0g_BhA");
+            //YouTubeVideo youtubeVideo = await youtube.GetVideoAsync(@"https://www.youtube.com/watch?v=__-vp0g_BhA");
+            //string guidYoutube = $"{Guid.Parse("7dc1b152-5e36-4a96-bb14-45773e6f8d14")}";
 
-            string filePath = $@"{AppDomain.CurrentDomain.BaseDirectory}{_config.LocalStorage.Youtube}{Guid.NewGuid()}{v.FileExtension}";
-            string outFileName = $@"{AppDomain.CurrentDomain.BaseDirectory}{_config.LocalStorage.Youtube}{Guid.NewGuid()}.mp3";
+            //string outFileName = $@"{AppDomain.CurrentDomain.BaseDirectory}{_config.LocalStorage.Youtube}{guidYoutube}.mp4";
+            string staticFileMp3 = $@"{AppDomain.CurrentDomain.BaseDirectory}{_config.LocalStorage.Youtube}sample-12s.mp3";
+            string staticOutFile = $@"{AppDomain.CurrentDomain.BaseDirectory}{_config.LocalStorage.Youtube}sample-12s.dat";
 
-            File.WriteAllBytes(filePath, v.GetBytes());
-
-            var errorCode = 0;
+            await RespondAsync("Звуки ада!");
 
             unsafe
             {
-                fixed (AVFormatContext** ppAvFormatInputFileContext = &_pAvFormatInputFileContext)
-                fixed (AVFormatContext** ppAvFormatOutputFileContext = &_pAvFormatOutputFileContext)
-                {
-                    errorCode = ffmpeg.avformat_open_input(ppAvFormatInputFileContext, filePath, null, null);
-                    ExitOnFfmpegError(errorCode, $"unable to open input file {filePath}");
-
-                    AVOutputFormat* AVOF = ffmpeg.av_guess_format("acc", null ,null);
-                    //AVOF->audio_codec = AVCodecID.AV_CODEC_ID_AAC;
-
-                    errorCode = ffmpeg.avformat_alloc_output_context2(ppAvFormatOutputFileContext, AVOF, null, outFileName);
-                    ExitOnFfmpegError(errorCode, $"unable to alloc context for output file {outFileName}");
-                }
-
-                CopyStreamInfo(_pAvFormatOutputFileContext, _pAvFormatInputFileContext);
-
-                // method can produce memory leaks if it is called from several threads concurrently. So if your app is multithreaded, please use lock on some static variable
-                errorCode = ffmpeg.avio_open(&_pAvFormatOutputFileContext->pb, outFileName, ffmpeg.AVIO_FLAG_WRITE);
-                ExitOnFfmpegError(errorCode, $"error while trying to opening file {outFileName} for writing");
-
-                errorCode = ffmpeg.avformat_write_header(_pAvFormatOutputFileContext, null);
-                ExitOnFfmpegError(errorCode, $"error while trying to write header of file {outFileName}");
-
-                CopyDataPackets(_pAvFormatOutputFileContext, _pAvFormatInputFileContext);
-
-                errorCode = ffmpeg.av_write_trailer(_pAvFormatOutputFileContext);
-                ExitOnFfmpegError(errorCode, $"error while trying to write trailer of file {outFileName}");
-
-                Cleanup();
+                FFmpegDy.decode_audio(staticFileMp3, staticOutFile);
             }
 
-            File.Delete(filePath);
-
+            await SendAsync(audio, staticOutFile);
         }
 
         private async Task SendAsync(IAudioClient client, string path)
         {
             // Create FFmpeg using the previous example
-            using (var ffmpeg = CreateStream(path))
-            using (var output = ffmpeg.StandardOutput.BaseStream)
+            //using (var ffmpeg = CreateStream(path))
+            //using (var output = ffmpeg.StandardOutput.BaseStream)
+            using (var output = new FileStream(path, FileMode.Open))
             using (var discord = client.CreatePCMStream(AudioApplication.Mixed))
             {
                 try { await output.CopyToAsync(discord); }
@@ -125,195 +98,6 @@ namespace Sudy_v3.Modules
             await RespondAsync($"Всем пока!");
 
             await channel.DisconnectAsync();
-        }
-
-        unsafe private static void PrintStreamInfo(int streamIndex, AVStream* pAvStream)
-        {
-            // There is av_dump_format function that produces almost the same result =)
-
-            Console.WriteLine($"Stream {streamIndex}");
-            Console.WriteLine($"\tType: {pAvStream->codecpar->codec_type}");
-            Console.WriteLine($"\tCodecId: {pAvStream->codecpar->codec_id}");
-
-            var pCodecDescriptor = ffmpeg.avcodec_descriptor_get(pAvStream->codecpar->codec_id);
-            string codecName, codecLongName;
-            if (pCodecDescriptor is not null)
-            {
-                codecName = FfmpegHelper.GetString(pCodecDescriptor->name);
-                codecLongName = FfmpegHelper.GetString(pCodecDescriptor->long_name);
-            }
-            else
-            {
-                codecName = codecLongName = "Unknown";
-            }
-
-            Console.WriteLine($"\tCodec name: {codecName}");
-            Console.WriteLine($"\tCodec long name: {codecLongName}");
-
-            Console.WriteLine($"\tBitrate: {pAvStream->codecpar->bit_rate}");
-
-            if (pAvStream->duration > 0)
-            {
-                var durationInSeconds = pAvStream->duration * ffmpeg.av_q2d(pAvStream->time_base);
-                Console.WriteLine($"\tDuration: {TimeSpan.FromSeconds(durationInSeconds)}");
-            }
-
-            if (pAvStream->codecpar->codec_type == AVMediaType.AVMEDIA_TYPE_VIDEO)
-            {
-                Console.WriteLine($"\tWidth: {pAvStream->codecpar->width}");
-                Console.WriteLine($"\tHeight: {pAvStream->codecpar->height}");
-
-                var avRationalAspectRatio = pAvStream->codecpar->sample_aspect_ratio;
-                Console.WriteLine($"\tSample aspect ratio: {avRationalAspectRatio.num} / {avRationalAspectRatio.den}");
-
-                Console.WriteLine($"\tLevel: {pAvStream->codecpar->level}");
-            }
-            else if (pAvStream->codecpar->codec_type == AVMediaType.AVMEDIA_TYPE_AUDIO)
-            {
-                Console.WriteLine($"\tChannels: {pAvStream->codecpar->channels}");
-                Console.WriteLine($"\tSample rate: {pAvStream->codecpar->sample_rate}");
-                Console.WriteLine($"\tFrame size: {pAvStream->codecpar->frame_size}");
-            }
-
-            foreach (var avDictionaryEntry in FfmpegHelper.GetEnumerator(pAvStream->metadata))
-            {
-                var key = FfmpegHelper.GetString(avDictionaryEntry.key);
-                var value = FfmpegHelper.GetString(avDictionaryEntry.value);
-                Console.WriteLine($"\t\t{key}: {value}");
-            }
-        }
-
-        unsafe private static void Cleanup()
-        {
-            if (_pAvFormatInputFileContext is not null)
-            {
-                fixed (AVFormatContext** ppAvFormatFileContext = &_pAvFormatInputFileContext)
-                    ffmpeg.avformat_close_input(ppAvFormatFileContext);
-            }
-
-            if (_pAvFormatOutputFileContext is not null)
-            {
-                fixed (AVFormatContext** ppAvFormatFileContext = &_pAvFormatOutputFileContext)
-                    ffmpeg.avformat_close_input(ppAvFormatFileContext);
-            }
-        }
-        private static void ExitOnFfmpegError(int ffmpegResultCode, string errorDescription)
-        {
-            if (ffmpegResultCode >= 0)
-                return;
-
-            var errorText = FfmpegHelper.GetErrorText(ffmpegResultCode);
-
-            ExitWithErrorText($"{errorDescription}. Ffmpeg error text: {errorText}");
-        }
-
-        private static void ExitWithErrorText(string errorText)
-        {
-            Console.WriteLine(errorText);
-
-            Cleanup();
-
-            Environment.Exit(-1);
-        }
-
-        unsafe private static void CopyStreamInfo(AVFormatContext* pAvFormatOutputFileContext, AVFormatContext* pAvFormatInputFileContext)
-        {
-            for (var i = 0; i < pAvFormatInputFileContext->nb_streams; i++)
-            {
-                var pAvInputStream = pAvFormatInputFileContext->streams[i];
-
-                if (pAvInputStream->codecpar->codec_type != AVMediaType.AVMEDIA_TYPE_AUDIO) continue;
-
-                PrintStreamInfo(i, pAvInputStream);
-
-                AVStream* pAvOutputStream = ffmpeg.avformat_new_stream(pAvFormatOutputFileContext, null);
-
-                if (pAvOutputStream is null)
-                    ExitWithErrorText($"Unable to create new stream #{i} for output file");
-
-                //var errorCode = ffmpeg.avcodec_parameters_copy(pAvOutputStream->codecpar, pAvInputStream->codecpar);
-
-                //ffmpeg.avcodec_open2(pAvFormatOutputFileContext, ffmpeg.avcodec_find_encoder_by_name("libfdk_aac"), null);
-
-                //ExitOnFfmpegError(errorCode, $"error while trying to copy stream #{i} parameters");
-
-                //AVCodecContext* encoder_ctx;
-
-                //encoder_ctx->codec_id = AVCodecID.
-
-                uint codecTag = 0;
-                if (ffmpeg.av_codec_get_tag2(pAvFormatOutputFileContext->oformat->codec_tag,
-                        pAvInputStream->codecpar->codec_id, &codecTag) == 0)
-                {
-                    Console.WriteLine(
-                        $"Warning: could not find codec tag for codec id {pAvInputStream->codecpar->codec_id}, default to 0.");
-                }
-
-                pAvOutputStream->codecpar->codec_tag = codecTag;
-
-                for (var j = 0; j < pAvInputStream->nb_side_data; j++)
-                {
-                    var avPacketSideData = pAvInputStream->side_data[j];
-
-                    var pAvOutPacketSideData =
-                        ffmpeg.av_stream_new_side_data(pAvOutputStream, avPacketSideData.type, avPacketSideData.size);
-
-                    if (pAvOutPacketSideData is null)
-                        ExitWithErrorText($"Unable to allocate side data structure for stream #{i} of output file");
-
-                    Buffer.MemoryCopy(avPacketSideData.data, pAvOutPacketSideData, avPacketSideData.size,
-                        avPacketSideData.size);
-                }
-
-                ffmpeg.av_dict_copy(&pAvOutputStream->metadata, pAvInputStream->metadata, ffmpeg.AV_DICT_APPEND);
-            }
-        }
-
-        unsafe private static void CopyDataPackets(AVFormatContext* pAvFormatOutputFileContext, AVFormatContext* pAvFormatInputFileContext)
-        {
-            AVPacket avPacket;
-            while (true)
-            {
-                ffmpeg.av_packet_unref(&avPacket);
-                var errorCode = ffmpeg.av_read_frame(pAvFormatInputFileContext, &avPacket);
-
-                if (errorCode == ffmpeg.AVERROR_EOF)
-                {
-                    break;
-                }
-                else
-                {
-                    ExitOnFfmpegError(errorCode, $"error while trying to read packet from input file");
-                }
-
-                if (avPacket.stream_index >= pAvFormatInputFileContext->nb_streams)
-                {
-                    Console.WriteLine(
-                        $"Warning: found new stream #{avPacket.stream_index} in input file. It will be ignored.");
-                    continue;
-                }
-
-                if (avPacket.flags.HasFlag(ffmpeg.AV_PKT_FLAG_CORRUPT))
-                {
-                    Console.WriteLine(
-                        $"Warning: corrupt packet found in stream #{avPacket.stream_index}.");
-                }
-
-                var pAvInputStream = pAvFormatInputFileContext->streams[avPacket.stream_index];
-                var pAvOutputStream = pAvFormatOutputFileContext->streams[avPacket.stream_index];
-
-                avPacket.pts = ffmpeg.av_rescale_q_rnd(avPacket.pts, pAvInputStream->time_base,
-                    pAvOutputStream->time_base, AVRounding.AV_ROUND_NEAR_INF | AVRounding.AV_ROUND_PASS_MINMAX);
-                avPacket.dts = ffmpeg.av_rescale_q_rnd(avPacket.dts, pAvInputStream->time_base,
-                    pAvOutputStream->time_base, AVRounding.AV_ROUND_NEAR_INF | AVRounding.AV_ROUND_PASS_MINMAX);
-                avPacket.duration =
-                    ffmpeg.av_rescale_q(avPacket.duration, pAvInputStream->time_base, pAvOutputStream->time_base);
-                avPacket.pos = -1;
-
-                ffmpeg.av_write_frame(pAvFormatOutputFileContext, &avPacket);
-            }
-
-            ffmpeg.av_packet_unref(&avPacket);
         }
     }
 }
